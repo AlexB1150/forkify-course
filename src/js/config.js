92207e68eb5a90334676457424b17e1ea7690cac
AlexB1150 <alexberryhill2001@gmail.com>
2025-03-31 16:49:16 -0500

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "ecb9bd34-b09a-4e21-8f49-a25ab3ed7ec8";
+export const MODAL_CLOSE_SEC = 2.5;
